ba2d8a6f4ca8ff56d7ae5a1c67c3b03f0bde9ac2
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-06-08 10:46:06 -0600

    Terminando metodos Crud en la Clase de controller

--- src/main/java/Api/Rest/CRUD/productos/ApiRest/controllers/ProductControllers.java
+++ src/main/java/Api/Rest/CRUD/productos/ApiRest/controllers/ProductControllers.java
@@ -1,14 +1,48 @@
 package Api.Rest.CRUD.productos.ApiRest.controllers;
 
+import Api.Rest.CRUD.productos.ApiRest.entity.Product;
+import Api.Rest.CRUD.productos.ApiRest.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/productos")
 public class ProductControllers {
     @Autowired
     ProductService productService;
+
+    @GetMapping
+    public List<Product> listarProducto(){
+        return productService.fileAll()
+    }
+
+    @GetMapping(value = "/{id}")
+    public Product obtenerProducto(@RequestParam Long id){
+        return productService.findById();
+    }
+
+    @PostMapping()
+    public void crearProducto(@RequestBody Product product){
+        productService.createProducto(product);
+    }
+
+    @PutMapping(value = "/{id}")
+    public void modificarProducto(@PathVariable Long id, @RequestBody Product product){
+        productService.modifyProducto(id, product);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void borrarProduct(@PathVariable Long id){
+        productService.deleteProducto(id);
+    }
+
+    @GetMapping(value = "/{nombre}")
+    public List<Product> buscarNombre(@RequestParam String nombre){
+        return productService.findByName(nombre);
+    }
 
 
 }
--- src/main/java/Api/Rest/CRUD/productos/ApiRest/dao/ProductDao.java
+++ src/main/java/Api/Rest/CRUD/productos/ApiRest/dao/ProductDao.java
@@ -1,4 +1,10 @@
 package Api.Rest.CRUD.productos.ApiRest.dao;
 
-public class ProductDao {
+import Api.Rest.CRUD.productos.ApiRest.entity.Product;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProductDao extends CrudRepository<Product, Long> {
+    List<Product> findByName(String nombre);
 }
