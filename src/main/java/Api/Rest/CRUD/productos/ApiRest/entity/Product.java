b45c310bee8a14176506b5894b90c05dfb7b5b99
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-06-08 09:54:10 -0600

    Creando Entidad - Product - Gets y Seths -

---
+++ src/main/java/Api/Rest/CRUD/productos/ApiRest/entity/Product.java
@@ -0,0 +1,69 @@
+package Api.Rest.CRUD.productos.ApiRest.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "productos")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String marca;
+    private String descripcion;
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Column(name = "crear_articulo")
+    @Temporal(TemporalType.DATE)
+
+    private Date creararticulo;
+
+    public Date getCreararticulo(){
+        return creararticulo;
+    }
+
+    public void setCreararticulo(Date creararticulo){
+        this.creararticulo = creararticulo;
+    }
+}
